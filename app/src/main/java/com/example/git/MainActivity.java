190b53658f0415f2090934998e8256281c746354
me <337144203@qq.com>
2019-06-03 11:10:04 +0800

    1

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("wutao");
-        System.out.println("w3");
+        System.out.println("w");
 
 
     }
